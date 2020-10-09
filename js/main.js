bc9a6457da5adb2f098910a98b2be166c639c3a2
Brandon-B91 <43705011+Brandon-B91@users.noreply.github.com>
2020-10-09 14:29:56 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+function modal() {
+    let modalBtn = document.querySelector('.covid');
+    modalBtn.addEventListener('click', function(event) {
+        let modal = document.querySelector('.modal');
+        modal.style.display = 'block';
+    });
+}
+modal();
+
+function outSideClick() {
+    window.addEventListener('click', (e) => {
+        let modal = document.querySelector('.modal');
+        if (e.target == modal) {
+            modal.style.display = 'none';
+        }
+    })
+}
+outSideClick();
+
+let open = document.querySelector('#open');
+let close = document.querySelector('#close');
+
+
+function toggleNav() {
+    const open = document.querySelector('#open');
+    const close = document.querySelector('#close');
+    const nav = document.querySelector('nav');
+
+    open.addEventListener('click', () => {
+        nav.style.display = 'block';
+        close.style.display = 'block';
+        open.style.display = 'none';
+    })
+
+    close.addEventListener('click', () => {
+        nav.style.display = 'none';
+        close.style.display = 'none';
+        open.style.display = 'block';
+    })
+}
+toggleNav();
